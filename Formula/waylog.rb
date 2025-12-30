60dfd9813c3725bdb36f8d24981e1f2c6d846b96
shayne-snap <shayne-snap@users.noreply.github.com>
2025-12-30 14:57:57 +0800

    fix: remove extra quotes in url

--- Formula/waylog.rb
+++ Formula/waylog.rb
@@ -1,7 +1,7 @@
 class Waylog < Formula
   desc "Seamlessly sync, preserve, and version-control your AI coding conversations locally"
   homepage "https://github.com/shayne-snap/waylog-cli"
-  url ""https://github.com/shayne-snap/waylog-cli/releases/download/v0.1.0/waylog-macos-arm64.tar.gz""
+  url "https://github.com/shayne-snap/waylog-cli/releases/download/v0.1.0/waylog-macos-arm64.tar.gz"
   sha256 "ab9c760dd97415c3ba09a29b28ccbf458f60c79552c32f23e434419a7b844d5b"
   version "v0.1.0"
   license "Apache-2.0"
